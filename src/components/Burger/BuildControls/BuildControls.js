55b1d1f18dca01e3f02f3ec1b303aa5d8dde791e
Adel Sadek <adel.sadek@linkdev.com>
2020-02-25 18:51:41 +0200

    added

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import BuildControl from './BuildControl/BuildControl';
+import classes from './BuildControls.css'
+
+const controls = [
+    { label: "Salad", type: 'salad'},
+    { label: "Meat", type: 'meat'},
+    { label: "Bacon", type: 'bacon'},
+    { label: "Cheese", type: 'cheese'}
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        { controls.map((ele, i)=>{
+           return  <BuildControl key={ele.label} Label={ele.label} added={()=> props.addedIngredients(ele.type)}/>
+        })}
+    </div>
+);
+
+export default buildControls;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import Aux from '../../hoc/Ax';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+
+
+const INGREDIENTS_PRICE = {
+    salad: 0.6,
+    meat: 1.3,
+    bacon: 1.1,
+    cheese: 0.5
+}
+
+class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 0,
+            meat: 1,
+            bacon: 0,
+            cheese: 0
+        },
+        totalPrice: 4
+    }
+
+    addIngredient = (type) => {
+        let updateIngredients = { ...this.state.ingredients };
+        updateIngredients[type] = updateIngredients[type] + 1;
+
+        let updatePrice = this.state.totalPrice;
+        updatePrice += INGREDIENTS_PRICE[type];
+
+        this.setState({ ingredients: updateIngredients, totalPrice: updatePrice });
+    }
+
+
+    render() {
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls addedIngredients={this.addIngredient} />
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
